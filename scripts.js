2e7b48d7641ec88354448d6d8f3e174bf54f1be6
Yadarsan <161267994+Yadarsan@users.noreply.github.com>
2024-09-10 12:19:47 +1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Smooth Scroll for Navigation Links
     const navLinks = document.querySelectorAll('.nav-links a');
     navLinks.forEach(link => {
         link.addEventListener('click', e => {
@@ -8,4 +9,39 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         });
     });
+
+    // Form Submission Handling
+    const form = document.querySelector('form[name="contact"]');
+    if (form) {
+        form.addEventListener('submit', handleSubmit);
+    }
 });
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const responseMessageElement = document.getElementById('responseMessage');
+
+    fetch('/', {
+        method: 'POST',
+        body: formData,
+        headers: { 'Accept': 'application/json' }
+    })
+    .then(response => {
+        console.log('Response Status:', response.status); // Log response status
+        return response.text(); // Read response as text
+    })
+    .then(text => {
+        console.log('Response Text:', text); // Log response text
+        if (text.includes('Message sent successfully')) {
+            responseMessageElement.textContent = 'Message sent successfully.';
+        } else {
+            throw new Error('Failed to send message.');
+        }
+    })
+    .catch(error => {
+        console.error('Fetch Error:', error); // Log fetch error
+        responseMessageElement.textContent = error.message;
+    });
+}
